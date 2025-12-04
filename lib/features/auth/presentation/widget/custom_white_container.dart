b6b7a65c810595661f4adea7bedab62f21215596
Qmar <qbakeer@dreamwd.com>
2025-12-04 14:42:09 +0300

    book marks and edites on regester and homepage

---
+++ lib/features/auth/presentation/widget/custom_white_container.dart
@@ -0,0 +1,22 @@
+import 'package:career/core/constant/class/app_color.dart';
+import 'package:career/core/constant/class/app_size.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomWhiteContainer extends StatelessWidget {
+  const CustomWhiteContainer({super.key, required this.icon, this.withRadius=false});
+final String icon;
+final bool withRadius;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(0.016.h(context)),
+      margin:withRadius? EdgeInsetsDirectional.only(end : 0.04.w(context)):  EdgeInsetsDirectional.only(end : 0.02.w(context)),
+      decoration: BoxDecoration(
+        color: AppColor.secondryColor,
+        borderRadius: withRadius? BorderRadius.circular(50): BorderRadius.circular(20),
+      ),
+     child: SvgPicture.asset(icon),
+    );
+  }
+}
---
+++ lib/features/book mark/presentation/advertisement_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdvertismentScreen extends StatelessWidget {
+  const AdvertismentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
