ba28d775fe932d8b128ca0779db2c6aa60f39e54
Batool Ramadan <batool.r@eternatech.io>
2026-03-29 19:17:25 +0300

    add firebase setting

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -7,12 +10,13 @@ plugins {
 
 android {
     namespace = "com.example.career"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "25.2.9519653"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -25,7 +29,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -37,6 +41,9 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:career/core/router/app_route.dart';
 import 'package:career/core/router/routes_name.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'core/localization/app_translation.dart';
+import 'core/notification/push_notification_services.dart';
 import 'core/theme/theme_manger.dart';
 import 'features/splash/presentation/getx/binding/splash_binding.dart';
+import 'firebase_options.dart';
 
 
-void main() async{
+ Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   //await AppTranslation.init();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessagingService().initNotificationsSettings();
   runApp(const MyApp());
 }
 
